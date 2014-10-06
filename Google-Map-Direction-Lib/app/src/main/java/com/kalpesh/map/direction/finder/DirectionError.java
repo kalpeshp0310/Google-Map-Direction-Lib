282102040fa62984478699968d3698caefca4fc9
Kalpesh Patel <kalpeshp0310@gmail.com>
2014-10-06 17:14:14 +0400

    Removed white spaces.

--- Google-Map-Direction-Lib/app/src/main/java/com/kalpesh/map/direction/finder/DirectionError.java
+++ Google-Map-Direction-Lib/app/src/main/java/com/kalpesh/map/direction/finder/DirectionError.java
@@ -5,10 +5,10 @@ package com.kalpesh.map.direction.finder;
  */
 public class DirectionError {
     public static final int NOT_FOUND = 1;
-    public static final int ZERO_RESULTS  = 2;
+    public static final int ZERO_RESULTS = 2;
     public static final int REQUEST_DENIED = 3;
     public static final int UNKNOWN_ERROR = 4;
-    public static final int INVALID_REQUEST  = 5;
-    public static final int NO_INTERNET_CONNECTION  = 6;
-    public static final int INVALID_RESPONSE  = 7;
+    public static final int INVALID_REQUEST = 5;
+    public static final int NO_INTERNET_CONNECTION = 6;
+    public static final int INVALID_RESPONSE = 7;
 }
